c7926405a88d155cb13e5b513e1d9fada8c1c99b
Demir Altay <77849115+daltay15@users.noreply.github.com>
2022-10-19 00:05:22 -0500

    Created rough draft website

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React from "react";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Signin from "./pages/Signin";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-8xl text-center">Meet Me Halfway</h1>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/signin" component={Signin} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/About.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function About() {
+  return (
+    <div className="text-8xl text-center text-bold">
+      <div className="flex flex-row justify-center bg-slate-400">
+        <Link to="/">
+          <button className="text-white hover:text-black font-bold py-2 px-6">
+            Home
+          </button>
+        </Link>
+        <Link to="/about">
+          <button className="text-black font-bold py-2 px-6 rounded">
+            About
+          </button>
+        </Link>
+        <Link to="/contact">
+          <button className=" text-white hover:text-black font-bold py-2 px-6 rounded">
+            Contact
+          </button>
+        </Link>
+        <Link to="/signin">
+          <button className="text-white hover:text-black font-bold py-2 px-6 rounded">
+            Login
+          </button>
+        </Link>
+      </div>
+      <div>
+        <h1 className="text-4xl mx-[150px] my-[75px]">
+          This is the about us page
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default About;
