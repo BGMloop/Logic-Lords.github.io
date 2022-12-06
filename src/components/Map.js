41051689ba92e337e69ff72669a0d5f185a95822
DajanekD <82248461+DajanekD@users.noreply.github.com>
2022-12-06 16:12:08 -0600

    Added LocationGetter

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,6 +2,8 @@ import React from "react";
 import { useCallback, useState, useEffect } from "react";
 import { GoogleMap, useJsApiLoader, Marker, Autocomplete} from "@react-google-maps/api";
 import API_KEY from "../API_KEY.json";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 const center = {
   lat: 33.25398070865379,
@@ -20,6 +22,24 @@ export default function Map() {
   });
 
   const [map, setMap] = useState(null);
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
+  const [status, setStatus] = useState(null);
+
+  const getLocation = () => {
+    if (!navigator.geolocation) {
+      setStatus('Geolocation is not supported by your browser');
+    } else {
+      setStatus('Locating...');
+      navigator.geolocation.getCurrentPosition((position) => {
+        setStatus(null);
+        setLat(position.coords.latitude);
+        setLng(position.coords.longitude);
+      }, () => {
+        setStatus('Unable to retrieve your location');
+      });
+    }
+  }
 
   const onLoad = useCallback(function callback(map) {
     const bounds = new window.google.maps.LatLngBounds(center);
@@ -49,13 +69,16 @@ export default function Map() {
           <div className="p-1 w-3/4">
             <div className="relative">
               <label for="name" className="text-4xl text-center text-gray-600">
+              <button onClick = {getLocation}>
+              <FontAwesomeIcon icon= {faLocationDot} />
+              </button>
                 Location 1
               </label>
               <Autocomplete>
               <input
                 type="address"
                 id="address"
-                placeholder="address 1"
+                placeholder="address 1" 
                 name="address"
                 className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
