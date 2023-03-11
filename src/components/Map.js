0c59a6ac2b7aac4e565f486d9f3f9d21873dc78b
Demir Altay <77849115+daltay15@users.noreply.github.com>
2023-03-11 13:14:48 -0600

    Revert "Attempt to beatifying the app"
    This reverts commit 68763b5e73b0904808b748fd67be4392de675d3b.

--- src/components/Map.js
+++ src/components/Map.js
@@ -10,6 +10,7 @@ import API_KEY from "../API_KEY.json";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
+
 const denton = {
   lat: 33.21618224226912,
   lng: -97.1332708813027,
@@ -71,14 +72,9 @@ export default function Map() {
   }, []);
 
   // handle location 1 and 2 submit button click
-  // if the route is a valid route continue else alert the user
   const locationClick = () => {
-    if (locationRef1.current.value && locationRef2.current.value) {
-      setLocation1(locationRef1.current.value);
-      setLocation2(locationRef2.current.value);
-    } else {
-      alert("Please enter a valid route");
-    }
+    setLocation1(locationRef1.current.value);
+    setLocation2(locationRef2.current.value);
   };
 
   // convert and pin location 1 and 2 on the map
@@ -181,15 +177,29 @@ export default function Map() {
   }
 
   return isLoaded ? (
-    <div className="flex flex-col items-center mt-2">
-      <div className="lg:w-1/2 md:w-2/3">
-        <div className="flex flex-col md:flex-row items-center text-center mb-4">
+    <div className="flex flex-col items-center mt-4">
+      <button
+        className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded-full mb-4"
+        onClick={getLocation}
+      >
+        Get Device location
+      </button>
+      <GoogleMap
+        mapContainerStyle={mapStyle}
+        center={denton}
+        zoom={5}
+        onLoad={(map) => setMap(map)}
+        onUnmount={onUnmount}
+      >
+        {/* Child components, such as markers, info windows, etc. */}
+        <Marker position={location1} />
+        <Marker position={location2} />
+      </GoogleMap>
+      <div className="lg:w-1/2 md:w-2/3 mt-4">
+        <div className="flex flex-col md:flex-row mb-8 items-center text-center">
           <div className="p-1 w-3/4">
             <div className="relative">
-              <label
-                for="name"
-                className="text-2xl md:text-4xl text-center text-gray-600"
-              >
+              <label for="name" className="text-4xl text-center text-gray-600">
                 Location 1
               </label>
               <Autocomplete>
@@ -204,36 +214,14 @@ export default function Map() {
                   id="address"
                   placeholder="address 1"
                   name="address"
-                  className="w-full mt-4 bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                 />
               </Autocomplete>
             </div>
           </div>
-          <button
-            className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-2 rounded-full mt-2 md:mb-12"
-            onClick={getLocation}
-          >
-            <svg
-              viewBox="0 0 24 24"
-              width="24"
-              height="24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="feather feather-map-pin"
-            >
-              <path d="M12 2C8.13 2 5 5.13 5 9c0 4.58 6.75 12.42 6.75 12.42a1 1 0 0 0 1.5 0C14.25 21.42 21 13.58 21 9c0-3.87-3.13-7-7-7z"></path>
-              <circle cx="12" cy="9" r="2.5"></circle>
-            </svg>
-          </button>
           <div className="p-1 w-3/4">
             <div className="relative">
-              <label
-                for="email"
-                className="text-2xl md:text-4xl text-center text-gray-600"
-              >
+              <label for="email" className="text-4xl text-gray-600">
                 Location 2
               </label>
               <Autocomplete>
@@ -248,30 +236,19 @@ export default function Map() {
                   id="address"
                   placeholder="address 2"
                   name="address"
-                  className="w-full mt-4 bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                 />
               </Autocomplete>
             </div>
           </div>
         </div>
         <button
-          className="flex mx-auto mt-2 mb-2 bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-12 rounded-full"
+          className="flex mx-auto mt-[-20px] mb-6 bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-12 rounded-full"
           onClick={locationClick}
         >
           Submit
         </button>
       </div>
-      <GoogleMap
-        mapContainerStyle={mapStyle}
-        center={denton}
-        zoom={5}
-        onLoad={(map) => setMap(map)}
-        onUnmount={onUnmount}
-      >
-        {/* Child components, such as markers, info windows, etc. */}
-        <Marker position={location1} />
-        <Marker position={location2} />
-      </GoogleMap>
     </div>
   ) : (
     <></>
